954a504bac696ac0e01c0e18cfebba7817ecf754
pgeovany <pgeovany@dcc.ufrj.br>
2022-06-29 17:17:48 -0300

    initial setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import GlobalCSS from "./globalStyles";
+
+ReactDOM.render(
+  <>
+    <GlobalCSS />
+    <App />
+  </>,
+  document.querySelector(".root")
+);
